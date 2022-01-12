b97103078a87bac934bab6232e156fc64a745282
Abigail Anne Calaminos <57781516+Abby367@users.noreply.github.com>
2022-01-12 23:03:38 +0800

    ~

---
+++ GoodsToGo/GoodsToGo/Views/FormController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoodsToGo.Views
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Forgot()
+        {
+            return View();
+        }
+    }
+}
