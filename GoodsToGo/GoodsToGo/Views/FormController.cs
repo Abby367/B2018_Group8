f09bfae088dedbb590e548bed962e7d44ea7ac19
Abigail Anne Calaminos <57781516+Abby367@users.noreply.github.com>
2022-01-12 20:02:52 +0800

    Project

---
+++ GoodsToGo/GoodsToGo/Views/FormController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GoodsToGo.Models;
+namespace GoodsToGo.Controllers
+{
+    public class FormController : Controller
+    {
+        Database1 db = new Database1();
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(user us)
+        {
+            db.users.Add(us);
+            db.SaveChanges();
+            return RedirectToAction("Login");
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(user us)
+        {
+            var obj = db.users.Where(x => x.User_Name.Equals(us.User_Name) && x.Password.Equals(us.Password)).FirstOrDefault();
+            if (obj != null)
+            {
+                return RedirectToAction("Employ", "Form");
+
+            }
+            else if (us.User_Name == "admin" && us.Password == "admin")
+            {
+                return RedirectToAction("Admin");
+            }
+            return View();
+        }
+       
+        public ActionResult Forgot()
+        {
+            return View();
+        }
+        public ActionResult Employ()
+        {
+            return View();
+        }
+
+    }
+}
